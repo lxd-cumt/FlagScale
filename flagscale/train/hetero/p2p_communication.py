fd48656cc608533cff6fa2798bad9754d1425c28
lxdcumt <1141051934@qq.com>
2025-12-28 00:48:02 +0800

    update

--- flagscale/train/hetero/p2p_communication.py
+++ flagscale/train/hetero/p2p_communication.py
@@ -25,18 +25,6 @@ def is_single_shape(x) -> bool:
         return True
     return False
 
-def get_device_type_for_comm(model_parallel_group=None):
-    device = 'cuda'
-    # "cpu:gloo": gloo only supports cpu tensor.
-    # "gloo" & "cpu:gloo,cuda:gloo": gloo supports both cpu and cuda tensor.
-    if isinstance(model_parallel_group, list):
-        if 'cpu:gloo' == torch.distributed.get_backend(model_parallel_group[0]):
-            device = 'cpu'
-    else:
-        if 'cpu:gloo' == torch.distributed.get_backend(model_parallel_group):
-            device = 'cpu'
-    return device
-
 def is_inter_mesh_comm(para_ctx: ParallelContext, comm_with_front_layer: bool):
     """ Judge if the p2p communication across meshes.
 
