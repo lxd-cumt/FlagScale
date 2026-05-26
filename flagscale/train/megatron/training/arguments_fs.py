51136983b3618378c0560c4d43d0be141a200767
lxd-cumt <1141051934@qq.com>
2026-05-26 19:20:32 +0800

    cp support for sliding window attention

--- flagscale/train/megatron/training/arguments_fs.py
+++ flagscale/train/megatron/training/arguments_fs.py
@@ -246,8 +246,10 @@ class FSTrainArguments:
         # DeepSeek-V4 Temporary
         if self.args.enable_hyper_connections:
             assert not self.args.overlap_moe_expert_parallel_comm, "Hyper-connection is not supported with overlap_moe_expert_parallel_comm yet!"
+        
         if self.args.experimental_attention_variant == "dsv4_hybrid":
-            assert self.args.context_parallel_size == 1, "Context parallelism is not supported with dsv4_hybrid attention variant yet!"
+            # assert self.args.context_parallel_size == 1, "Context parallelism is not supported with dsv4_hybrid attention variant yet!"
+            assert not self.args.apply_rope_fusion, "Contiguous Split for embedding is not support with fused rope"
 
     def post_validate_args(self):
         """Post-validate the arguments after Megatron function `validate_args`."""
